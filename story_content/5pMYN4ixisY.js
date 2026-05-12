4f8582ce13935d7773999fd7e0e9880bdd2b5fa8
RaghavendraT <torriraghub4u@gmail.com>
2026-05-12 12:36:16 +0530

    Delete story_content directory

--- story_content/5pMYN4ixisY.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideSvgData('svg-5pMYN4ixisY', "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24px\" viewBox=\"0 -960 960 960\" width=\"24px\" fill=\"#FFFFFF\" id=\"svg-5pMYN4ixisY\"><g pointer-events=\"visiblePainted\" data-accepts=\"events\"><path d=\"M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z\" /></g></svg>");
